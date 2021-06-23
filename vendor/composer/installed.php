e4eaebc6ff821ccee64f322ba8bd1990fa97f948
SxTI <caique_rechi@hotmail.com>
2021-06-23 19:52:11 -0300

    teste 2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-SxTI',
+        'version' => 'dev-SxTI',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '11883723285acd6ae893564d9b58b5db0d5d6a4c',
+        'name' => 'pc-gamer/php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pc-gamer/php' => array(
+            'pretty_version' => 'dev-SxTI',
+            'version' => 'dev-SxTI',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '11883723285acd6ae893564d9b58b5db0d5d6a4c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
